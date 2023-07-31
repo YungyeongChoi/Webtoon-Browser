3d4db1f2f57fd04a295cd67a133020a4ea516c7b
Kate <fef7005@gmail.com>
2023-07-31 10:19:36 -0500

    Futures

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:webtoon/models/webtoon_detail_model.dart';
+import 'package:webtoon/models/webtoon_episode_model.dart';
 import 'package:webtoon/models/webtoon_model.dart';
 
 class ApiService {
@@ -23,15 +24,26 @@ class ApiService {
     throw Error();
   }
 
-  static Future<List<WebtoonEpisodeMode>> getLatestEpisodesById(
+  static Future<WebtoonDetailModel> getToonById(String id) async {
+    // List<WebtoonEpisodeModel> episodesInstances = [];
+    final url = Uri.parse("baseUrl/$id");
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      final webtoon = jsonDecode(response.body);
+      return WebtoonDetailModel.fromJson(webtoon);
+    }
+    throw Error();
+  }
+
+  static Future<List<WebtoonEpisodeModel>> getLatestEpisodesById(
       String id) async {
-    List<WebtoonEpisodeMode> episodesInstances = [];
+    List<WebtoonEpisodeModel> episodesInstances = [];
     final url = Uri.parse("baseUrl/$id/episodes");
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final episodes = jsonDecode(response.body);
       for (var episode in episodes) {
-        episodesInstances.add(WebtoonDetailModel.fromJson(episode));
+        episodesInstances.add(WebtoonEpisodeModel.fromJson(episodes));
       }
       return episodesInstances;
     }
